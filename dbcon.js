e5fd59e6f2737480a96008c989d2c961ab94c320
chenhowa <chenhowa@oregonstate.edu>
2017-06-20 23:48:29 -0700

    modified the config file for the local server on my computer

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_chenhowa',
+  host            : 'localhost',
+  user            : 'chenhowa',
   password        : 'apples',
-  database        : 'cs340_chenhowa'
+  database        : 'your_name'
 });
 
 module.exports.pool = pool;
